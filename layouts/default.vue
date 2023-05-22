e61da9b15beb87cf6ffcf7bd0c5b045b972df221
Sopanvil <lenivkov12@gmail.com>
2023-05-22 22:57:55 +0300

    [master] start

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="header">
+      <div class="header__container">
+        <div class="header-links">Magput</div>
+        <div class="header-search"></div>
+      </div>
+    </div>
+    <div class="">
+        <slot />
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss"></style>
