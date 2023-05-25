dca8c4c438902715a8200387499c6d076626e189
sopanv <kovalev@magput.ru>
2023-05-25 19:46:09 +0300

    [main] modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: [
-        '@/assets/styles/main.scss',
-      ]
-})
+    css: ['@/assets/styles/main.scss'],
+    plugins: [
+      {
+          src:'~/plugins/v-calendar', mode: 'client'
+      }
+  ],
+});
--- pages/index.vue
+++ pages/index.vue
@@ -7,23 +7,59 @@
                         И в любой день недели вы можете присоединиться к нашему однодневному или многодневному туру, не
                         важно, путешествуете вы большой компанией или в одиночку
                     </div>
+                    <div class="search">
+                        <div class="search__container">
+                            <!-- 1 -->
+                            <div class="search-header">
+                                <div class="search-type"></div>
+                                <div class="search__all">
+                                    <input type="checkbox" id="search-all">
+                                    <label for="search-all">искать везде</label>
+                                </div>
+                            </div>
+                            <!-- 2 -->
+                            <div class="search-input">
+                                <client-only>
+                                    <v-date-picker mode="range" :min-date="new Date()" :attributes="attributes">
+                                        <input type="text">
+                                    </v-date-picker>
+                                </client-only>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
         <section class="search">
             <div class="search__container">
-                <div v-for="(item, index) in users" :key="index" class="search-item">
+                <!-- <div v-for="(item, index) in users" :key="index" class="search-item">
                     {{ item.name }}
-                </div>
+                </div> -->
             </div>
         </section>
     </div>
 </template>
 <script>
+
 export default {
+    setup() {
+        const attributes = ref([
+            {
+                visibility: 'click',
+                placement: 'bottom',
+                dates: {
+                    start: new Date(),
+                },
+            },
+        ]);
+        return {
+            attributes
+        }
+    },
     data() {
         return {
             users: '',
+            selectedDate: '',
         };
     },
     async mounted() {
@@ -33,7 +69,6 @@ export default {
 </script>
 <style lang="scss" scoped>
 @import '@/assets/styles/variables.scss';
-
 .intro {
     width: 100%;
     display: flex;
