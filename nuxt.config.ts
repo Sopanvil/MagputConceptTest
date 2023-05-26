71e97dab5bbf0d633579ce6bbb733ae149e9fa4b
sopanv <kovalev@magput.ru>
2023-05-26 20:05:12 +0300

    [main] code api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: ['@/assets/styles/main.scss'],
+    css: ['@/assets/styles/main.scss', '@/assets/styles/variables.scss'],
     plugins: [
-      {
-          src:'~/plugins/v-calendar', mode: 'client'
-      }
-  ],
+        {
+            src: '~/plugins/v-calendar',
+            mode: 'client',
+        },
+    ],
 });
